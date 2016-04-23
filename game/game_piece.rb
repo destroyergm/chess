e9e2b70477121eb0746acacae6f85d4fa3e5165d
destroyergm <stefan_vg@hotmail.com>
2016-04-23 13:45:19 +0200

    Add GamePiece class

---
+++ game/game_piece.rb
@@ -0,0 +1,14 @@
+class GamePiece
+
+	attr_accessor :type, :color
+
+
+	def initialize (type, game_symbol)
+		@type = type
+		@game_symbol = game_symbol
+	end
+
+	# Inherited method in subclasses to get valid moves list
+	def get_available_moves 
+	end
+end
