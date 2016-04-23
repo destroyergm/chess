6cf2ad4f26d18ee065d3ded83c4686bdbd58dca3
destroyergm <stefan_vg@hotmail.com>
2016-04-23 13:52:44 +0200

    Add initialize method for King

--- game/game_piece.rb
+++ game/game_piece.rb
@@ -1,7 +1,6 @@
 class GamePiece
 
-	attr_accessor :type, :color
-
+	attr_accessor :type, :color, :position
 
 	def initialize (type, game_symbol)
 		@type = type
@@ -9,6 +8,6 @@ class GamePiece
 	end
 
 	# Inherited method in subclasses to get valid moves list
-	def get_available_moves 
+	def get_available_moves
 	end
 end
--- game/king.rb
+++ game/king.rb
@@ -1,5 +1,8 @@
 require_relative "game_piece"
 
 class King < GamePiece
-	
+	def initialize(position)
+		super :king, "K"
+		@position = position
+	end
 end
