340a3ac4e60607334abdb332ebd0c0792d1a2d93
destroyergm <stefan_vg@hotmail.com>
2016-04-25 00:20:31 +0200

    Add pawn_spec.rb file

---
+++ spec/pawn_spec.rb
@@ -0,0 +1,18 @@
+require_relative "../game/pawn.rb"
+
+describe Pawn do
+
+	it "Creates a new white piece at a given position" do
+		pawn = Pawn.new [2,3], "White"
+		expect(pawn.position).to eql [2,3]
+		expect(pawn.color).to eql "White"
+		expect(pawn.game_symbol).to eql "♙"
+	end
+
+	it "Creates a new black piece at a given position" do
+		pawn = Pawn.new [3,2], "Black"
+		expect(pawn.position).to eql [3,2]
+		expect(pawn.color).to eql "Black"
+		expect(pawn.game_symbol).to eql "♟"
+	end
+end
