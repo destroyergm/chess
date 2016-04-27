a263f90464cadfe332f991823ba0d422c5466c1c
destroyergm <stefan_vg@hotmail.com>
2016-04-27 19:39:31 +0200

    Add Point class

--- game/util.rb
+++ game/util.rb
@@ -1,5 +1,23 @@
 class Util 
 	def self.from_to(p1,p2)
-		[0,0]
+		start = Point::from_array(p1)
+		finish = Point::from_array(p2)
+		moves = []	
+
+		
+		moves
+	end
+end
+
+class Point
+	attr_accessor :x, :y
+
+	def initialize(x,y)
+		@x, @y = x, y
+	end
+
+	def self.from_array(array)
+		raise "InvalidArrayCount" if array.count != 2
+		p = Point.new array[0], array[1]
 	end
 end
